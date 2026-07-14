53f5299c9618c7df751fd41d339601878b86d8a0
rivaldjr39 <rivaldoako25@gmail.com>
2026-07-14 14:59:06 +0300

    feat context app

--- mg/itu/rivaldo/controller/FrontControllerServlet.java
+++ mg/itu/rivaldo/controller/FrontControllerServlet.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import org.springframework.web.context.WebApplicationContext;
 
 
 @WebServlet(name = "FrontControllerServlet", urlPatterns = {"/"})
@@ -21,7 +22,7 @@ public class FrontControllerServlet extends HttpServlet {
     private String suffixe;
     private List<String> controllerClassNames;
     private Map<UrlType, Mapping> mappingUrls = new HashMap<>();
-   
+    private WebApplicationContext springContext;
 
     @Override   
     @SuppressWarnings("unchecked") 
@@ -32,6 +33,7 @@ public class FrontControllerServlet extends HttpServlet {
           controllerClassNames = (List<String>) context.getAttribute("controllerClassNames");
           prefixe = (String) context.getAttribute("prefixe");
           suffixe = (String) context.getAttribute("suffixe");
+          springContext = (WebApplicationContext) context.getAttribute("springContext");
         } catch (Exception e) {
             throw new ServletException("Erreur initialisation", e);
         }
@@ -49,8 +51,17 @@ public class FrontControllerServlet extends HttpServlet {
 
             if (mapping != null) {
                 Object controllerInstance = mapping.getControllerClass().getDeclaredConstructor().newInstance();
-                Object retour = mapping.getMethod().invoke(controllerInstance);
-    
+                Object retour;
+                //verification si la méthode a des paramètres
+                Class<?>[] parameterTypes = mapping.getMethod().getParameterTypes();
+                if(parameterTypes.length == 0){
+                    retour = mapping.getMethod().invoke(controllerInstance);
+                }else if(parameterTypes.length == 1 && WebApplicationContext.class.isAssignableFrom(parameterTypes[0])){
+                    retour = mapping.getMethod().invoke(controllerInstance, springContext);
+                }else {
+                    out.println("La méthode " + mapping.getMethod().getName() + " de la classe " + mapping.getControllerClass().getSimpleName() + " a des paramètres non supportés.");
+                    return;
+                }
                 if(retour instanceof ModelAndVue) {
                     ModelAndVue modelAndVue = (ModelAndVue) retour;
                     
--- mg/itu/rivaldo/controller/Listner.java
+++ mg/itu/rivaldo/controller/Listner.java
@@ -5,6 +5,9 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,10 +18,10 @@ import mg.itu.rivaldo.annotation.Url;
 
 @WebListener
 public class Listner implements ServletContextListener {
-
     public void contextInitialized(ServletContextEvent sce) {
         try {
             ServletContext context = sce.getServletContext();
+            WebApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(context);
             String packageName = context.getInitParameter("packageNames");
             String prefixe = context.getInitParameter("prefixe");
             String suffixe = context.getInitParameter("suffixe");
@@ -36,6 +39,7 @@ public class Listner implements ServletContextListener {
             context.setAttribute("controllerClassNames", controllerClassNames);
             context.setAttribute("prefixe", prefixe);
             context.setAttribute("suffixe", suffixe);
+            context.setAttribute("springContext", springContext);
 
             System.out.println("Framework initialisé.");
 
